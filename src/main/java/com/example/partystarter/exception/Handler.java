ab9a135ce2a2446387451d8dd2f8b36bad88d4c8
dakiman <dvancov@hotmail.com>
2022-05-22 05:28:21 +0200

    handle validation exceptions

--- src/main/java/com/example/partystarter/exception/Handler.java
+++ src/main/java/com/example/partystarter/exception/Handler.java
@@ -1,9 +1,15 @@
 package com.example.partystarter.exception;
 
 import com.example.partystarter.model.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class Handler {
@@ -12,6 +18,14 @@ public class Handler {
     public ResponseEntity<?> handleException(ResourceException e) {
         return ResponseEntity
                 .status(e.getHttpStatus())
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<?> handleException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+//                TODO extract actual validation message
                 .body(new ErrorResponse(e.getMessage()));
     }
 }
--- src/main/java/com/example/partystarter/model/request/PostPartyRequest.java
+++ src/main/java/com/example/partystarter/model/request/PostPartyRequest.java
@@ -3,12 +3,12 @@ package com.example.partystarter.model.request;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class PostPartyRequest {
-    @NotNull
+    @Size(min = 1, message = "You must choose at least one drink")
     private List<Integer> drinks;
     @Length(min = 3, max = 40)
     private String name;
